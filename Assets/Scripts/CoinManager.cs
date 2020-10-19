f4291bbe6e79b6913ed7a5ceee7448602b271da8
Nutchapol <48310831+nutchapolkitsathan@users.noreply.github.com>
2020-10-19 22:12:40 +0700

    First commit
    First commit for this game

---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinManager : MonoBehaviour
+{
+    public GameObject coinObject;
+    private float coin = 0;
+
+
+    public void AddCoin()
+    {
+        coin++;
+        Debug.Log(coin);
+    }
+
+    public void SpawnCoin()
+    {
+        Instantiate(coinObject, new Vector3(Random.Range(-3.0f,3.0f), 0.2f, Random.Range(-3.0f, 3.0f)),Quaternion.identity);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnCoin();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private CoinManager coinManager;
+    private float speed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinManager = GameObject.Find("CoinManager").GetComponent<CoinManager>();
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        controller.Move(move * Time.deltaTime * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Coin")
+        {
+            Destroy(other.gameObject);
+            coinManager.SpawnCoin();
+            coinManager.AddCoin();
+        }
+    }
+}
